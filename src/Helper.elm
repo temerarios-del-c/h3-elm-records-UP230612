d7537f9610a541d6fd55dbff251f3d37f69ba679
UP230612 <up230612@alumnos.upa.edu.mx>
2026-02-15 20:07:40 -0600

    Solución de ejercicios de records y funciones en Helper.elm

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,136 @@
 module Helper exposing (..)
+
+import Html exposing (Html, div, h1, li, text, ul)
+
+
+
+-- SIMPLE EXERCISES
+
+
+add2 : Int -> Int -> Int
+add2 int1 int2 =
+    int1 + int2
+
+
+add3 : Float -> Float -> Float -> Float
+add3 f1 f2 f3 =
+    f1 + f2 + f3
+
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc int1 int2 operator =
+    operator int1 int2
+
+
+
+-- RECORDS EXERCISES
+
+
+type alias Language =
+    { name : String
+    , releaseYear : Int
+    , currentVersion : String
+    }
+
+
+languages : List Language
+languages =
+    [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }
+    , { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
+    ]
+
+
+languageNames : List Language -> List String
+languageNames list =
+    List.map .name list
+
+
+
+-- RECORDS EXERCISES
+
+
+type alias User =
+    { name : String
+    , uType : String
+    }
+
+
+users : List User
+users =
+    [ { name = "Roberto", uType = "Student" }
+    , { name = "Mitsiu", uType = "Professor" }
+    ]
+
+
+onlyStudents : List User -> List String
+onlyStudents list =
+    List.map
+        (\user ->
+            if user.uType == "Student" then
+                user.name
+
+            else
+                ""
+        )
+        list
+
+
+
+-- ALIASES EXERCISE
+
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+
+
+videogames : List Videogame
+videogames =
+    [ { title = "Control", releaseYear = 2019, available = True, downloads = 1000000, genres = [ "Action", "Shooter" ] }
+    , { title = "Ocarina of Time", releaseYear = 1998, available = True, downloads = 7000000, genres = [ "Action", "Adventure" ] }
+    ]
+
+
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres list =
+    List.map .genres list
+
+
+
+-- HTML EXERCISE: COMPUTER
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16GB"
+    , model = "Nitro 5"
+    , brand = "Acer"
+    , screenSize = "15.6"
+    }
+
+
+main : Html msg
+main =
+    div []
+        [ h1 [] [ text "My laptop" ]
+        , div []
+            [ ul []
+                [ li [] [ text ("Ram: " ++ myLaptop.ram) ]
+                , li [] [ text ("Modelo: " ++ myLaptop.model) ]
+                , li [] [ text ("Marca: " ++ myLaptop.brand) ]
+                , li [] [ text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
